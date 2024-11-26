73c7f0876fda9678ecb096a039bffd8c1f88d354
Daniil-Tomaylyi <daniiltomajly@gmail.com>
2024-11-26 17:35:09 +0300

    second commit

--- app/src/main/java/com/example/testapp/presentation/vacancy/VacancyFragment.kt
+++ app/src/main/java/com/example/testapp/presentation/vacancy/VacancyFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.testapp.R
 import com.example.testapp.databinding.FragmentVacancyBinding
 
@@ -13,11 +16,18 @@ class VacancyFragment : Fragment() {
 
     private lateinit var binding: FragmentVacancyBinding
 
+    private lateinit var navHostFragment: NavHostFragment
+
+    private lateinit var navController: NavController
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_vacancy, container, false)
+        navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.NavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
         return binding.root
     }
 }
